878338b8bba3453a6e81f720568a3dafa95f7264
imikhaylov <IMikhaylov@alfabank.ru>
2024-08-25 16:19:47 +0300

    clean

--- src/main/java/com/dragon/ai/bot/DemoChatAiApplication.java
+++ src/main/java/com/dragon/ai/bot/DemoChatAiApplication.java
@@ -1,7 +1,6 @@
 package com.dragon.ai.bot;
 
 import com.dragon.ai.bot.configuration.property.BotProperties;
-import com.dragon.ai.bot.configuration.TelegramHandlerConfigurations;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -13,9 +12,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 public class DemoChatAiApplication {
 
     public static void main(String[] args) {
-        var ctx = SpringApplication.run(DemoChatAiApplication.class, args);
-
-        ctx.getBean(TelegramHandlerConfigurations.class);
+        SpringApplication.run(DemoChatAiApplication.class, args);
     }
 
 }
